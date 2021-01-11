cd17bf1ef2faacdd0e66c5a522769a00414862fe
多啦ABCD梦 <819110812@qq.com>
2021-01-11 21:59:47 +0800

    swiper and recommend-view

---
+++ src/views/home/ChildrenComp/HomeSwiper.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <Swiper>
+      <swiper-item v-for="item in banners">
+        <a :href="item.link">
+          <img :src="item.image" alt="">
+        </a>
+      </swiper-item>
+    </Swiper>
+  </div>
+</template>
+
+<script>
+import {Swiper,SwiperItem} from '@/components/common/Swiper'
+import {getHomeMultiData} from "@/network/home";
+
+export default {
+  name: "HomeSwiper",
+  props:{
+    banners:{
+      type:Array,
+      default(){
+        return []
+      }
+    }
+  },
+  components:{
+    Swiper,
+    SwiperItem
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
